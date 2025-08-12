b8a75417c03e49df0eb1e5072820db2b9f23fc59
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-12 13:39:30 -0400

    Primer commit

---
+++ src/test/java/com/example/nativequerytutorialsrequests/NativeQueryTutorialsRequestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nativequerytutorialsrequests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeQueryTutorialsRequestsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
